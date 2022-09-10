0fbf8c0ad92debf376eb620243a8fb969ca1cbc8
tovach <red5.dalv@gmail.com>
2022-09-10 15:37:34 +0300

    feat: add styled components

---
+++ components/buttons/primary/PrimaryButton.tsx
@@ -0,0 +1,6 @@
+import { FC } from 'react';
+import { Wrapper } from './PrimaryButton.styles';
+
+type PrimaryButtonProps = {};
+
+export const PrimaryButton: FC<PrimaryButtonProps> = () => <Wrapper>PrimaryButton</Wrapper>;
--- containers/aside/Aside.tsx
+++ containers/aside/Aside.tsx
@@ -1,5 +1,10 @@
 import { FC } from 'react';
+import { PrimaryButton } from '@components/buttons';
 
 type AsideProps = {};
 
-export const Aside: FC<AsideProps> = () => <div>Aside</div>;
+export const Aside: FC<AsideProps> = () => (
+  <div>
+    <PrimaryButton />
+  </div>
+);
